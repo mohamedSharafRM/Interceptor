8d32c6dbdcfeb8231c72ec7e330aa4283b93f0cd
Mohamed Sharaf <134208355+mohamedSharafRM@users.noreply.github.com>
2026-05-20 01:32:27 +0300

    Show all products including soft deleted items

--- Program.cs
+++ Program.cs
@@ -1,21 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Interceptor.Data.Contexts;
 using Interceptor.Models;
+using Microsoft.EntityFrameworkCore;
 
 using var dbContext = new ApplicationDbContext();
 
-List<Product> newProducts =
-    [
-        new Product { Name = "Product 1", Description = "Description 1", Price = 10.00m },
-        new Product { Name = "Product 2", Description = "Description 2", Price = 20.00m },
-        new Product { Name = "Product 3", Description = "Description 3", Price = 30.00m }
-    ];
-
-
-dbContext.Products.AddRange(newProducts);
-dbContext.SaveChanges();
-
-var products = dbContext.Products.ToList();
+var products = dbContext.Products.IgnoreQueryFilters().ToList();
 foreach (var product in products)
 {
     Console.WriteLine($"Name: {product.Name}");
