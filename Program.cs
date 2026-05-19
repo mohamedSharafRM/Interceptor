1123c713a79f2f83ef9e2c64a6f53d1e0e02d6f0
Mohamed Sharaf <134208355+mohamedSharafRM@users.noreply.github.com>
2026-05-20 01:27:06 +0300

    delete a product (soft deleted)

--- Program.cs
+++ Program.cs
@@ -4,27 +4,14 @@ using Interceptor.Models;
 
 using var dbContext = new ApplicationDbContext();
 
-var product1 = new Product
-{
-    Name = "Laptop",
-    Description = "Gaming Laptop",
-    Price = 19500m
-};
+var firstProduct = dbContext.Products.FirstOrDefault();
 
-dbContext.Products.Add(product1);
+if (firstProduct == null)
+    return;
+
+dbContext.Products.Remove(firstProduct);
 dbContext.SaveChanges();
 
-Console.WriteLine("Product added successfully.");
-Console.WriteLine($"Product ID: {product1.Id}");
-Console.WriteLine($"Product Name: {product1.Name}");
-Console.WriteLine($"Product Description: {product1.Description}");
-Console.WriteLine($"Product Price: {product1.Price.ToString("N0")}");
-Console.WriteLine($"Product Created At: {product1.CreatedAt}");
-Console.WriteLine($"Product Updated At: {product1.UpdatedAt}");
-Console.WriteLine($"Product IsDeleted: {product1.IsDeleted}");
 
-product1.Price = 25000m;
-dbContext.SaveChanges();
-Console.WriteLine("Product updated successfully.");
-Console.WriteLine($"Product Price: {product1.Price.ToString("N0")}");
-Console.WriteLine($"Product updated at : {product1.UpdatedAt}");
+Console.WriteLine("Product Deleted");
+Console.WriteLine($"IsDeleted: {firstProduct.IsDeleted}");
