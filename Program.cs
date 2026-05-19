14da3d318adb1ad9adaceae7351628e0e1266d44
Mohamed Sharaf <134208355+mohamedSharafRM@users.noreply.github.com>
2026-05-20 01:16:47 +0300

    adding a new product

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Interceptor.Data.Contexts;
+using Interceptor.Models;
+
+using var dbContext = new ApplicationDbContext();
+
+var product1 = new Product
+{
+    Name = "Laptop",
+    Description = "Gaming Laptop",
+    Price = 19500m
+};
+/*
+dbContext.Products.Add(product1);
+dbContext.SaveChanges();
+
+Console.WriteLine("Product added successfully.");
+Console.WriteLine($"Product ID: {product1.Id}");
+Console.WriteLine($"Product Name: {product1.Name}");
+Console.WriteLine($"Product Description: {product1.Description}");
+Console.WriteLine($"Product Price: {product1.Price.ToString("N0")}");
+Console.WriteLine($"Product Created At: {product1.CreatedAt}");
+Console.WriteLine($"Product Updated At: {product1.UpdatedAt}");
+Console.WriteLine($"Product IsDeleted: {product1.IsDeleted}");
+*/
+
